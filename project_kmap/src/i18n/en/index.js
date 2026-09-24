4d08b04a70bb652e221ceda9ed98d852687d70af
huybndc <huybndc@gmail.com>
2026-09-24 21:27:28 +0900

    Xoá bớt phần không cần thiết, đã chuyển sang repo khác

--- project_kmap/src/i18n/en/index.js
+++
@@ -1,6 +0,0 @@
-import { common } from './common.js';
-import { ch1 } from './ch1.js';
-import { ch2 } from './ch2.js';
-import { ch3 } from './ch3.js';
-
-export const en = { ...common, ...ch1, ...ch2, ...ch3 };
--- project_kmap/src/ui/kmap-state.js
+++
@@ -1,4 +0,0 @@
-/* State dùng chung của tab K-map. Tách riêng để kmap-page.js và
-   kmap-steps.js cùng đọc/ghi mà không import vòng. */
-
-export const K = { n: 4, values: new Array(16).fill(0), view: null, groups: [], steps: null, stepIdx: -1 };
